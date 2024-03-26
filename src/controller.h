f35308733da8f13a8eaa264bdca809dcb0c5ac97
Gabin Dudillieu <gabin.dudillieu@etu.u-paris.fr>
2024-03-26 09:43:01 +0100

    chore(controller): Add missing comments of controller

--- src/controller.h
+++ src/controller.h
@@ -5,9 +5,16 @@ typedef enum ACTION { NONE, UP, DOWN, LEFT, RIGHT, QUIT } ACTION;
 
 #include "./model.h"
 
+/** Depending on the press key on the keyboard, returns the action, or adds the character to chat_line
+ */
 ACTION control();
 
+/** Initialize view, controller and model to start a game
+ */
 int init_game();
+
+/** Game loop
+ */
 int game();
 
-#endif  // SRC_CONTROLLER_H_
+#endif // SRC_CONTROLLER_H_
