0acc00ce6b0faf0ed5acbc66809c08449c463403
Gabin Dudillieu <gabin.dudillieu@etu.u-paris.fr>
2024-04-19 23:35:50 +0200

    feat(communication_server): Add functions to communicate the game with the client

--- src/communication_server.h
+++ src/communication_server.h
@@ -1,3 +1,6 @@
+#include <arpa/inet.h>
+#include <stdint.h>
+
 #ifndef SRC_COMMUNICATION_SERVER_H_
 #define SRC_COMMUNICATION_SERVER_H_
 
@@ -6,8 +9,11 @@
 
 int send_connexion_information(int sock, GAME_MODE mode, int id, int eq, int port_udp, int portmdiff,
                                uint16_t adrmdiff[8]);
+int send_game_board(int sock, struct sockaddr_in6 *addr_mult, uint16_t num, board *board_);
+int send_game_update(int sock, struct sockaddr_in6 *addr_mult, int num, tile_diff *diff, uint8_t nb);
 
 initial_connection_header *recv_initial_connection_header(int sock);
 ready_connection_header *recv_ready_connexion_header(int sock);
+game_action *recv_game_action(int sock);
 
 #endif // SRC_COMMUNICATION_SERVER_H_
